2b52842282e1661abafec1573aec46385e18b585
root <root@CacharreoASI.arostegui.proxmox>
2026-05-11 10:30:11 +0000

    Acabada la pratica

--- P3/ejercicio5.c
+++ P3/ejercicio5.c
@@ -12,16 +12,23 @@ int main(int argc, char *argv[]){
     size_t tam = 1024;
     void *shm_ptr;
     
-    int idsem = semget(clave, 1, 0666);
+    int idsem = semget(clave, 2, 0666);
     int idshm = shmget(clave, tam, 0666); 
 
-    struct sembuf operacion;
-    operacion.sem_num = 0;  // El índice del semáforo (solo hay 1, así que es el 0)
-    operacion.sem_op = -1;  // IMPORTANTE: -1 significa "quiero pillar el semáforo y bloquearlo"
-    operacion.sem_flg = 0;  // Banderas a 0 y a correr
+    struct sembuf entrada[2];
 
-    // Y ejecutas la operación (1 es la cantidad de operaciones que le pasas, o sea, 1)
-    semop(idsem, &operacion, 1);
+    // Operación para el semáforo -1
+    entrada[0].sem_num = 0;
+    entrada[0].sem_op = -1;
+    entrada[0].sem_flg = 0;
+
+    // Operación para el semáforo 1
+    entrada[1].sem_num = 1;
+    entrada[1].sem_op = -1;
+    entrada[1].sem_flg = 0;
+
+    // Llamamos a semop pasándole el array de 2 operaciones
+    semop(idsem, entrada, 2); 
 
     shm_ptr = shmat(idshm, NULL, 0);
     int *p_enteros = (int *)shm_ptr;
@@ -32,10 +39,12 @@ int main(int argc, char *argv[]){
     p_enteros[0] = secreto1;
     sleep(3);
 
+    struct sembuf salida[2];
 
+    salida[0].sem_num = 0; salida[0].sem_op = 1; salida[0].sem_flg = 0;
+    salida[1].sem_num = 1; salida[1].sem_op = 1; salida[1].sem_flg = 0;
 
-    operacion.sem_op = 1; // IMPORTANTE: 1 significa "suelto el semáforo, que pase el siguiente"
-    semop(idsem, &operacion, 1);
+    semop(idsem, salida, 2);
 
     shmdt(shm_ptr);
 
